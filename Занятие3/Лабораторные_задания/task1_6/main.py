844b3026fafce7f2eece24538336a3cdd8358072
mhelll <mhelll666@gmail.com>
2021-10-21 21:07:29 +0300

    init Практическая работа 21.10

--- Занятие3/Лабораторные_задания/task1_6/main.py
+++ Занятие3/Лабораторные_задания/task1_6/main.py
@@ -1,3 +1,6 @@
+def pow_func(a, b):
+
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+
--- Занятие3/Практические_задания/task2_1/main.py
+++ Занятие3/Практические_задания/task2_1/main.py
@@ -1,10 +1,14 @@
 def pow_func(base, pow_=2):
     # base ** pow_ -> реализовать через цикл while
-    ...  # TODO
+    res = 1
+    while pow_ > 0:
+        res = res * base
+        pow_ -= 1
+
+    return res
 
 
 if __name__ == "__main__":
-    a = 5
-    n = 3
-
+    a = 4
+    n = 2
     print(pow_func(a, n))
